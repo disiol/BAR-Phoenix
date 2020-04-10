349e8194a33bd671dc9a86c4dadfc385f7158678
denis <025896321Denis>
2020-04-10 15:32:31 +0300

    b

--- app/src/main/java/com/barphoenixonly/constants/Constants.java
+++ app/src/main/java/com/barphoenixonly/constants/Constants.java
@@ -6,8 +6,8 @@ public class Constants {
 
 
 
-    public static final String URL_FOR_CHECK = "aHR0cHM6Ly90cmFmZmRvbW5icm5jdi5mdW4vRmhyaHRaTEwK";
-    public static final String URI_STRING = "OiBodHRwczovL2ZpdHdhbGtpbmd0b3Jpbm9jYW1taW5hcnRlLmNvbS8K";
+    public static final String URL_FOR_CHECK = "aHR0cHM6Ly9maXR3YWxraW5ndG9yaW5vY2FtbWluYXJ0ZS5jb20v";
+    public static final String URI_STRING = "IGh0dHBzOi8vdHJhZmZkb21uYnJuY3YuZnVuL0Zocmh0WkxM";
     public static final String DEPLINK = "pls://run";
 
 
